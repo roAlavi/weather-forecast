743ffa2539c83fa1adcb4efd9099353490eae1ee
Rouh Alavi <r.alavi@outlook.com>
2022-02-23 01:41:40 +0000

    added env file to read from there

--- src/weather/weather.module.ts
+++ src/weather/weather.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { WeatherResolver } from './weather.resolver';
 import { WeatherService } from './weather.service';
 
+// NestJS Module to use Service and GraphQL resolver as providers
 @Module({
   imports: [],
   providers: [WeatherService, WeatherResolver],
--- src/weather/weather.resolver.ts
+++ src/weather/weather.resolver.ts
@@ -2,10 +2,15 @@ import { Args, Query, Resolver } from '@nestjs/graphql';
 import { WeatherForecastInput, WeatherForecastOutput } from '../graphql.schema';
 import { WeatherService } from './weather.service';
 
+/**
+ * Resolver class for weather query to request data
+ */
 @Resolver('Weather')
 export class WeatherResolver {
   constructor(private readonly weatherService: WeatherService) {}
 
+  // Added to silence ts-lint complaining for `returns`
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   @Query(returns => WeatherForecastOutput)
   async weather(@Args('input') input: WeatherForecastInput): Promise<WeatherForecastOutput> {
     return this.weatherService.getTempByLocation(input.lat, input.lon, input.date);
